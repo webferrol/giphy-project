bbe2f235cdad13b5c6c38997ba5bf659f242227a
webferrol <xurxo@webferrol.com>
2023-06-02 23:28:20 +0200

    Crea pendiente la creación de custom hook

--- src/GiphyApp.jsx
+++ src/GiphyApp.jsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react'
 import { CategoriesList, AddCategory } from './components'
 import PropTypes from 'prop-types'
 import uuid4 from 'uuid4'
-import './App.css'
 
 function GiphyApp ({ title = 'Título de ejemplo' }) {
   const [categories, setCategories] = useState([])
@@ -14,11 +13,11 @@ function GiphyApp ({ title = 'Título de ejemplo' }) {
   }
 
   return (
-    <>
+    <main className='ontainer mx-auto px-4'>
       <h1 className='my-5 text-5xl text-center lg:text-7xl'>{title}</h1>
       <AddCategory onAddCategory={handleAddCategory} />
       <CategoriesList categoriesAll={categories} />
-    </>
+    </main>
   )
 }
 
--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,31 +1,50 @@
 import PropTypes from 'prop-types'
 import { getGifs } from '../helpers/getgifs'
 import { useState, useEffect } from 'react'
+import Bars from './Bars'
 
 export function GifGrid ({ category }) {
   const [data, setData] = useState([])
   const [isLoading, setIsLoading] = useState(true)
 
-  const fetchData = async () => {
-    setData(await getGifs(category))
+  useEffect(function () {
+    const fetchData = async () => {
+      setData(await getGifs(category))
+      setIsLoading(false)
+    }
+    fetchData()
+  }, [category])
+
+  if (isLoading) {
+    return (
+      <div className='flex justify-center'>
+        <Bars />
+      </div>
+    )
   }
 
-  useEffect(function () {
-    fetchData()
-    setIsLoading(false)
-  }, [isLoading])
-
-  if (isLoading) return (<>Cargando</>)
-
   return (
-    <div className='container mx-auto space-y-2 md:space-y-0 md:gap-2 md:grid md:grid-cols-2 lg:space-y-0 lg:gap-2 lg:grid lg:grid-cols-3'>
+    <div className='mx-auto space-y-2 md:space-y-0 md:gap-2 md:grid md:grid-cols-2 lg:space-y-0 lg:gap-2 lg:grid lg:grid-cols-3'>
       {
         data?.map(function (gif) {
           const { id, title, url } = gif
           return (
-            <img className='object-cover w-full h-32 rounded shadow-2xl' key={id} src={url} alt={title} />
+            <img
+              className='object-cover w-full h-32 rounded shadow-2xl'
+              key={id}
+              src={url}
+              alt={title}
+            />
           )
         })
+      }
+
+      {
+        !data.length && (
+          <div className='p-4 mb-4 text-sm text-green-800 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400' role='alert'>
+            <span className='font-medium'>Oh no!</span> No se han econtrado resultados.
+          </div>
+        )
       }
     </div>
   )
